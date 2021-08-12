e262a2de59d803f2ebd16a5ccbf0a74f8780f693
Emilio Dominguez <emilio.dominguez@live.com.ar>
2021-08-11 23:23:45 -0300

    Updates

--- src/index.jsx
+++
@@ -1,6 +0,0 @@
-import { render } from "react-dom";
-import reportWebVitals from "./reportWebVitals";
-import App from "./App";
-
-render(<App />, document.getElementById("root"));
-reportWebVitals();
